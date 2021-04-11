ef5e7be29e4e0f546f081e06460484b9663732e9
Daniel Leicht <daniel.leicht@flixbus.com>
2021-04-11 17:47:08 +0200

    removed leftover

--- controller/src/adapter/gobot.go
+++ controller/src/adapter/gobot.go
@@ -23,9 +23,6 @@ const (
 
 	// user control led
 	pin_user_control_led = "15"
-
-	// button to switch to schedule
-	pin_schedule_button = "16"
 )
 
 // i2c address configuration
@@ -46,13 +43,10 @@ type Gobot struct {
 	// TODO: abstract output components as well
 	StatusLed      *tbot.RgbLedDriver
 	HeatingRelay   *gpio.RelayDriver
-	UserControlLed *gpio.LedDriver
 
 	bme280         *i2c.BME280Driver
-	scheduleButton *gpio.ButtonDriver
 
 	OnNewReading     func(*data.Reading)
-	OnScheduleButton func()
 }
 
 // NewGobotAdapter creates a new adaptor for tempsens using gobot.
@@ -63,11 +57,8 @@ func NewGobotAdapter(adaptor gobot.Adaptor, name string) *Gobot {
 		adaptor:          &adaptor,
 		StatusLed:        tbot.NewRgbLedDriver(adaptor.(gpio.DigitalWriter), pin_status_rgb_red, pin_status_rgb_green, pin_status_rgb_blue),
 		HeatingRelay:     gpio.NewRelayDriver(adaptor.(gpio.DigitalWriter), pin_heating_relay),
-		UserControlLed:   gpio.NewLedDriver(adaptor.(gpio.DigitalWriter), pin_user_control_led),
 		bme280:           i2c.NewBME280Driver(adaptor.(i2c.Connector), i2c.WithBus(i2c_bme280_bus), i2c.WithAddress(i2c_bme280_address)),
-		scheduleButton:   gpio.NewButtonDriver(adaptor.(gpio.DigitalReader), pin_schedule_button),
 		OnNewReading:     func(*data.Reading) {},
-		OnScheduleButton: func() {},
 	}
 }
 
@@ -75,21 +66,12 @@ func (d *Gobot) Boot() error {
 	return gobot.NewRobot(
 		d.Name,
 		[]gobot.Connection{*d.adaptor},
-		[]gobot.Device{d.StatusLed, d.HeatingRelay, d.bme280, d.UserControlLed, d.scheduleButton},
+		[]gobot.Device{d.StatusLed, d.HeatingRelay, d.bme280},
 		func() {
 			// poll reading once and then in the configured polling rate
 			d.pollReading()
 
 			gobot.Every(reading_polling_rate, d.pollReading)
-
-			// handle button release
-			err := d.scheduleButton.On(gpio.ButtonRelease, func(s interface{}) {
-				d.OnScheduleButton()
-			})
-
-			if err != nil {
-				panic(err)
-			}
 		},
 	).Start()
 }
